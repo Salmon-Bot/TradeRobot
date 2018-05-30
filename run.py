a8beb84ade57c944b6444837563adcde64e3092d
wangmaodong <ahbbhywmd@163.com>
2018-05-30 17:34:10 +0800

    调整工程目录，新建Strategym目录，机器人监听策略等

--- run.py
+++ run.py
@@ -1,8 +1,7 @@
-from service import rest_api_service
-from service import websocket_subscribe_service
+from exchangeapi.huobi import rest_api
 
 if __name__ == "__main__":
     #websocket.start()
-    print(rest_api_service.get_kline('ethusdt',"1min",200));
+    print(rest_api.get_kline('ethusdt',"1min",200));
     
     print('hello');
